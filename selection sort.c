7ba8ce483195f808b0f07374fe2b28f96526577f
beli444eve <beli444eve@gmail.com>
2015-04-21 22:15:10 +0800

    Selection Sort

--- selection sort.c
+++ selection sort.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-	int i, N, *v;
+	int i,j, N, *v;
 	srand(time(0));
 	N=10;
 	v=(int*)malloc(N*sizeof(int));
@@ -14,5 +14,34 @@ int main()
 		printf("%d,",v[i]);
 	}
 	printf("\n");
+	
+	//selection sort
+	//掃描一遍所有數字，找到最小值，挪至陣列左端。
+	//遞迴處理尚未排序的 N-1 個元素。
+	
+	int min,temp;
+	
+	for(i=0;i<N;++i)		//遞迴N-1次 
+	{
+		min=i;
+		
+		for(j=i;j<N;++j)   	//找最小值 
+		{	                
+			if(v[min]>v[j])
+				min=j;		
+		}
+		
+		temp=v[min];     	//交換 
+		v[min]=v[i];
+		v[i]=temp;		
+	}
+	
+	for(i=0;i<N;++i)
+	{
+		printf("%d,",v[i]);
+	}
+	
+	
+	
 	return 1;
 }
